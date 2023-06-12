13c296f6efd4f9560064307b19ee64b97111e668
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2023-06-12 13:58:21 +0200

    Connecting models to the django admin panel and adding example items ( just a few )

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Note
+from .models import *
 
 
 admin.site.register(Note)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Cart)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
@@ -7,12 +8,14 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     desciption = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
 
