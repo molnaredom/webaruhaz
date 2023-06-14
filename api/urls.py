20bce5e0abbaeefe8cc766b458f036e144ee6af1
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2023-06-14 11:21:37 +0200

    set up JWT in the api/django side for authentatication

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path('products/', views.getProducts, name="products"),
     path('cart/', views.getCart, name="cart"),
     path('categories/', views.getCategories, name="categories"),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
